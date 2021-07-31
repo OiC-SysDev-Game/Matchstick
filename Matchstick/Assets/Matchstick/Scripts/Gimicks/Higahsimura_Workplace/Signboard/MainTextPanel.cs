12403de2d4e05d9f9afec975039dd12c61b8f360
y-Higashimura <B9319@oic.jp>
2021-07-31 17:01:27 +0900

    看板作業中 .csをシフトジスからUTF8に変更の為の一時保存

---
+++ Matchstick/Assets/Matchstick/Scripts/Gimicks/Higahsimura_Workplace/Signboard/MainTextPanel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTextPanel : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Signboard;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Matchstick/Assets/Matchstick/Scripts/Gimicks/Higahsimura_Workplace/Signboard/Signboard.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signboard : MonoBehaviour
+{
+	[SerializeField] private GameObject MainTextPanel;
+
+}
