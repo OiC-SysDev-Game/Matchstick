8311d110c77188d7636cab9ab5321280dea19a9a
y-Higashimura <B9319@oic.jp>
2021-08-08 16:11:49 +0900

    茨にPlayerが近づくとしたから生えてくるアニメーションを追加

--- Matchstick/Assets/Matchstick/Scripts/Gimicks/Thorn/ThornAnimation.cs
+++ Matchstick/Assets/Matchstick/Scripts/Gimicks/Thorn/ThornAnimation.cs
@@ -7,19 +7,28 @@ using UnityEngine.Experimental.Rendering.Universal;
 public class ThornAnimation : MonoBehaviour
 {
 	public float AnimTime = 1.0f;
+	public float ActiveDistance = 2.0f;
 
+	private GameObject player;
 	private Vector3 position;
-
-	public void StartAnimation()
-	{
-		StartCoroutine("Animation");
-	}
+	private bool IsActivated;
 
 	private void Start()
 	{
+		player = GameObject.Find("Player");
 		position = transform.position;
 		var down = new Vector3(0, transform.lossyScale.y, 0);
 		transform.position = position - down;
+		IsActivated = false;
+	}
+
+	private void Update()
+	{
+		if(!IsActivated && Vector3.Distance(player.transform.position, this.transform.parent.position) <= ActiveDistance)
+		{
+			IsActivated = true;
+			StartCoroutine("Animation");
+		}
 	}
 
 	private IEnumerator Animation()
@@ -30,8 +39,13 @@ public class ThornAnimation : MonoBehaviour
 		{
 			yield return null;
 			time += Time.deltaTime;
-			transform.position = Vector3.Lerp(startPosition, position, time / AnimTime);
+			transform.position = Vector3.Lerp(startPosition, position, easeOutQuint(time / AnimTime));
 		}
 		transform.position = position;
+	}
+
+	float easeOutQuint(float t)
+	{
+		return 1 - Mathf.Pow(1 - t, 5);
 	}
 }
